c0f30c279895681add71f6479a37896252ee53f4
huang47work <377298496@qq.com>
2017-03-30 18:11:33 +0800

    init push

---
+++ src/main/java/com/mason/doc2pdf/JodDemo.java
@@ -0,0 +1,26 @@
+package com.mason.doc2pdf;
+
+import com.artofsolving.jodconverter.DocumentConverter;
+import com.artofsolving.jodconverter.openoffice.connection.OpenOfficeConnection;
+import com.artofsolving.jodconverter.openoffice.connection.SocketOpenOfficeConnection;
+import com.artofsolving.jodconverter.openoffice.converter.OpenOfficeDocumentConverter;
+
+import java.io.File;
+
+/**
+ * Created by huangsiqian on 2017/3/30 0030.
+ */
+public class JodDemo {
+    public static void main(String[] args) throws Exception {
+        File inputFile = new File("E:/haha.doc");
+        File outputFile = new File("E:/hehe.pdf");
+// connect to an OpenOffice.org instance running on port 8100
+        OpenOfficeConnection connection = new SocketOpenOfficeConnection(8100);
+        connection.connect();
+// convert
+        DocumentConverter converter = new OpenOfficeDocumentConverter(connection);
+        converter.convert(inputFile, outputFile);
+// close the connection
+        connection.disconnect();
+    }
+}
